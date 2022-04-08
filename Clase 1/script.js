41ecd5f4e91e78336bed61a8df58ff0881534fd8
Francisco Pugh <franciscopugh01@gmail.com>
2022-04-07 23:55:44 -0300

    Clase 1

---
+++ Clase 1/script.js
@@ -0,0 +1,55 @@
+// Esto es un comentario
+/*
+
+    Esto es un comentario de multiples lineas
+
+*/
+
+/**
+ * Number String Boolean null undefined
+ 
+
+var nombreApellido = 'Francisco Pugh'
+var nombre_apellido = "Francisco Pugh"
+
+var nombreApellido = 5
+
+let edad = 45
+edad = "Y es medio viejo"
+
+console.log(edad)
+
+
+let producto = 100
+const IVA = 1.21
+
+//IVA = 1.23
+console.log(100 * IVA)
+
+let numero1 = 5
+let numero2 = "Hola"
+
+console.log(numero1 * numero2)
+*/
+//NaN (Not a Number)
+/*
+    5 + 5 = 10 (Number)
+    "5" + "5" = "55" (String)
+    5 + "5" = "55" (String)
+    "5" + 5 = "55" (String)
+*/
+
+/*
+    "5" * 5 = 25 (Number)
+
+
+
+let numero1 = Number(prompt("Ingrese el numero"))
+let numero2 = parseFloat(prompt("Ingrese otro numero"))
+
+console.log(numero1 + numero2)
+*/
+
+let numero3 = parseInt(prompt("Ingrese otroooo numero"))
+
+console.log(numero3)
